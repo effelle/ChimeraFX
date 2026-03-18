422c1cc68378e15e824ec3e26da207bbb909b9e9
effelle <effelle@gmail.com>
2026-03-18 16:20:20 -0300

    Update cfx_sequence __init__.py

--- components/cfx_sequence/__init__.py
+++ components/cfx_sequence/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import light, select, sensor, number, button, text_sensor
+from esphome.components import light, select, event, sensor, number, button, text_sensor
 from esphome import automation
 from esphome.const import (
     CONF_ID,
